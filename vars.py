750801426dd779c45b2e169b475f570bd81a61ef
pwbhairepo11 <pwbhairepo@gmail.com>
2025-06-01 21:47:54 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,15 +1,15 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+#@yesabp 
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "xxxxxxxxx"))
-API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxxxxx")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "5680454765"))
-CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
-AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+API_ID = int(environ.get("API_ID", "28460032"))
+API_HASH = environ.get("API_HASH", "1457c3ba64719a1e442aae67217b67c2")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7346044260:AAFYhCt4La8N42GshgLHgy6nCvcdFpQKqcM")
+OWNER = int(environ.get("OWNER", "6977768796"))
+CREDIT = "@yesabp"
+AUTH_USER = os.environ.get('AUTH_USERS', '6977768796').split(',')
+AUTH_USERS = [int(6977768796) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
